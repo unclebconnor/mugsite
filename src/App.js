db07b04c9d5211dbc539180fcf9b6de4c2db8362
unclebconnor <brian.connor@datadoghq.com>
2018-12-25 21:03:46 -0500

    some fonts

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
           <div id="stars-group-5"></div>
           <div id="stars-group-6"></div>
           <header>
-            <h2>The Top</h2>
+            <h1 className="site-header">Mug Site</h1>
           </header>
           <nav>
             <Link className="nav-link" to="/">Home</Link>
