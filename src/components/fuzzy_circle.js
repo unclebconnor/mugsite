f43c750d1077fc6d885db9772342a38d95282c76
unclebconnor <brian.connor@datadoghq.com>
2019-05-25 12:16:52 -0700

    removed warp as it was looping funny

--- src/components/fuzzy_circle.js
+++ src/components/fuzzy_circle.js
@@ -4,16 +4,29 @@ import "../style/App.scss";
 class FuzzyCircle extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			requestID: null,
+			activeWarp: null
+		};
 
 		this.makeSound = this.makeSound.bind(this);
+		this.stopMakingSound = this.stopMakingSound.bind(this);
 	};
 
 	makeSound = e => {
-		console.log('making sound');
+		var circle = e.target;
+		var s = circle.parentElement;
+		///add class to svg to animate
+	}
+
+	stopMakingSound = e => {
+		var circle = e.target;
+		var s = circle.parentElement;
+		///remove class to svg to animate
 	}
 
 	render() {
+		console.log('state: ',this.state)
 		return (
 			<div>
 				<div className="circle-wrapper rel">
@@ -30,6 +43,7 @@ class FuzzyCircle extends React.Component {
 							fill="lightgrey"
 							opacity="0.15"
 							onMouseOver={e => this.makeSound(e)}
+							onMouseLeave={e => this.stopMakingSound(e)}
 							filter="url(#blurMe)"
 						/>
 					</svg>
